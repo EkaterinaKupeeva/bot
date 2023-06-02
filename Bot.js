32daf0542c23d6059f80c949d0080eb07d23583e
EkaterinaKupeeva <135067402+EkaterinaKupeeva@users.noreply.github.com>
2023-06-02 07:12:12 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -12,26 +12,26 @@
 
 let keywords = ["символ бесконечности", "млечный путь", "галактика"];
 let keyword = keywords[getRandom(0, keywords.length)];
-let name = document.getElementsByName("text");
+let name = document.getElementById("text");
 let b = new MouseEvent("click", name);
 let button = document.getElementsByTagName('button')[3];
 let links = document.links;
 
-    function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-if (name !== undefined) {
-name[0].value = keyword;
-name.b;
-button.click();
-} else { 
-  for(let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("wikipedia.org") != -1) {
-      let link = links[i];
-      console.log("Нашел строку " + link);
-      location.href = link.href;
-      break;
+if (name != null) {
+    name.value = keyword;
+    name.b;
+    button.click();
+} else {
+    for(let i = 0; i < links.length; i++) {
+        if (links[i].href.indexOf("wikipedia.org") != -1) {
+            let link = links[i];
+            console.log("Нашел строку " + link);
+            location.href = link.href;
+            break;
+        }
     }
-  }
 }
